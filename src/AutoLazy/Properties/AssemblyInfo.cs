1ad88ac762a0a79e743cbec90b3f50ad500b1697
Brandon Cuff <brandoncuff@gmail.com>
2015-07-13 19:41:55 -0700

    Added stuff for creating nuget package

--- src/AutoLazy/Properties/AssemblyInfo.cs
+++ src/AutoLazy/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AutoLazy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Fody add-in that implements the double check locking pattern automatically.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Brandon Cuff")]
 [assembly: AssemblyProduct("AutoLazy")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
