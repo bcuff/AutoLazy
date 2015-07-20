e3d4c453756c8a840a822344260bb42c62fbaf13
Brandon Cuff <brandoncuff@gmail.com>
2015-07-20 12:05:42 -0700

    Made [Lazy] Inherited = false

--- src/AutoLazy/LazyAttribute.cs
+++ src/AutoLazy/LazyAttribute.cs
@@ -6,7 +6,7 @@ namespace AutoLazy
     /// <summary>
     /// Marks the specified method for lazy loading using the double check locking pattern.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false)]
     public class LazyAttribute : Attribute
     {
     }
